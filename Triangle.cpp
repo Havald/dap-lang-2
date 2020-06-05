ec1ced7c79c23c1ac6c9a9c5e1b8ac878266076d
Havald <0malte.wild@gmail.com>
2020-06-05 18:54:53 +0200

    Überprüfung, ob Dreieck aus verschiedenen Punkten besteht

--- Triangle.cpp
+++ Triangle.cpp
@@ -7,7 +7,11 @@ public:
 		if(TheSimplex.size() != 3) {
 			throw "Could not initialize triangle: wrong dimension";
 		}
+		if((this->at(0) == this->at(1)) || (this->at(0) == this->at(2)) || (this->at(1) == this->at(2))) {
+			throw "Could not initialize triangle: All Points need to be different";
+		}
 	}
+
 	double Girth() {
 		Point firstPoint = this->at(0), secondPoint = this->at(1), thirdPoint = this->at(2);
 		return firstPoint.EuclidDistanceTo(secondPoint) + secondPoint.EuclidDistanceTo(thirdPoint) + thirdPoint.EuclidDistanceTo(firstPoint);
