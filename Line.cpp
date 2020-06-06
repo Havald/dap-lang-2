7615d15a9785d91c07a6a3cc8620fc34d573d90b
Havald <0malte.wild@gmail.com>
2020-06-06 13:06:27 +0200

    Line Kommentare
    Etwas angepasst

--- Line.cpp
+++ Line.cpp
@@ -2,7 +2,7 @@
 #include "PointArray.cpp"
 
 #define ACC 1e-9
-#define SGN(x) ((x)< -ACC ? -1 : (x) > ACC ? 1 : 0) // Die Definition SGN gibt das Vorzeichen einer Fließkommazahl mit der Genauigkeit ACC zurück
+#define SGN(x) ((x)< -ACC ? -1 : (x) > ACC ? 1 : 0) // Die Definition SGN gibt das Vorzeichen von x mit der Genauigkeit ACC zurück
 
 class Line : private PointArray {
 public:
@@ -16,12 +16,12 @@ public:
 		return ((*this)[0] - (*this)[1]); 
 	}
 
-	int PointLeft(const Point &To) { // Berechnet, ob ein Punkt Links von der gerichteten Linie P1->P2 liegt (ähnlich ThisPointLeftOfMe, dort allerdings links von P2->P1).  
+	int PointLeft(const Point &To) { // Berechnet, ob ein Punkt Links von der gerichteten Linie P1->P2 liegt.  
 	
 		double value = ((*this)[0][1] - (*this)[1][1]) * To[0] + 	// Die Berechnung erfolgt über den Umlaufsinn, den das Dreieck aus der Linie und dem Punkt To hat.
 					((*this)[1][0] - (*this)[0][0]) * To[1] + 		// Wenn Value Positiv ist, wird das Dreieck mathematisch positiv durchlaufen, To liegt also links.
 					((*this)[0][0]*(*this)[1][1]-(*this)[1][0]*(*this)[0][1]); // Ist Value 0, sind sie kollinear.
 					
-		return SGN(value); // Die Überprüfung findet mit einer Bestimmten genauigkeit statt, um die Ungenauigkeit von doubles auszugleichen. 
+		return SGN(value); // Die Überprüfung findet mit einer bestimmten Genauigkeit statt, um die Ungenauigkeit von doubles auszugleichen. 
 	}
 }; 
